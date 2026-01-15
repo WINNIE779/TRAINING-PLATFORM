cc3d71040eaa87ca61c5b7d5f1f47472e9f070dd
WINNIE.X <winnie.xiao@sjfood.com>
2026-01-15 20:44:34 +0800

    Init the project

---
+++ training/src/router/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Home } from "../pages/home";
+import { Route, Routes } from "react-router-dom";
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
