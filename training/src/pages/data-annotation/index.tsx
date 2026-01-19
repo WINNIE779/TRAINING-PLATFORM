9ad391f98e561dc1545b71497497681de6965208
WINNIE.X <winnie.xiao@sjfood.com>
2026-01-19 20:54:29 +0800

    Add the Router

---
+++ training/src/pages/data-annotation/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DataAnnotation = () => {
+  return <div>Data Annotation Page</div>;
+};
--- training/src/router/index.tsx
+++ training/src/router/index.tsx
@@ -1,11 +1,44 @@
-import React from "react";
-import { Home } from "../pages/home";
+import { DataAnnotation } from "@/pages/data-annotation";
+import { DataManagement } from "@/pages/data-management";
+
+import { SimulationTraining } from "@/pages/simulation-training";
+import { ReactElement } from "react";
+
 import { Route, Routes } from "react-router-dom";
+
+export interface IRouteItem {
+  key: string;
+  path: string;
+  element: ReactElement;
+  children?: IRouteItem[];
+}
+
+const routesList: IRouteItem[] = [
+  {
+    key: "data-management",
+    path: "/data-management",
+    element: <DataManagement />,
+  },
+
+  {
+    key: "data-annotation",
+    path: "/data-annotation",
+    element: <DataAnnotation />,
+  },
+  {
+    key: "simulation-training",
+    path: "/simulation-training",
+    element: <SimulationTraining />,
+  },
+];
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<DataManagement />} />
+      {routesList.map((route) => (
+        <Route key={route.key} path={route.path} element={route.element} />
+      ))}
     </Routes>
   );
 };
